f7cd63c2290d7f69d0c250d7b7d0b5c539969332
Gwen <gwendall.esnault@gmail.com>
2015-07-21 22:11:00 +0200

    last

--- server/publications.js
+++ server/publications.js
@@ -1,4 +1,4 @@
-Meteor.publish(null, function(limit) {
+Meteor.publish(null, function() {
   var posts = Posts.find();
   var userIds = posts.map(function(p){ return p.userId });
   var users = Meteor.users.find({_id: {$in: userIds}});
